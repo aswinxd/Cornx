a0766c7a62af4c092711aa85aaea5af523678e1a
aswinxd <155294690+aswinxd@users.noreply.github.com>
2024-04-11 15:03:26 +0530

    Update events.py

--- FallenRobot/events.py
+++ FallenRobot/events.py
@@ -82,7 +82,9 @@ def bot(**args):
     if pattern is not None and not pattern.startswith("(?i)"):
         args["pattern"] = "(?i)" + pattern
 
-    args["pattern"] = pattern.replace("^/", r_pattern, 1)
+    if pattern:
+        args["pattern"] = pattern.replace("^/", r_pattern, 1)
+
     stack = inspect.stack()
     previous_stack_frame = stack[1]
     file_test = Path(previous_stack_frame.filename)
